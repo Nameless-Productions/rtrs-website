8bc99a875d1f0ad8c2ad78360e65ad21da15c9ca
thenamelessdev <nameless@thenamelessdev.com>
2026-04-26 10:05:54 +0200

    feat: add navbar and some pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import MainPage from './components/MainPage'
+import WishlistPage from './components/WishlistPage';
 
 export default function App() {
+  const [page, setPage] = useState("home");
   return (<>
-    <p className='font-bold'>Hello world</p>
+    <nav className='flex border-b-2 border-b-white p-2 space-x-3'>
+      <button onClick={() => setPage("home")} className='text-2xl font-bold cursor-pointer'>Right to Roam <span className='text-yellow-400'>Studios</span></button>
+
+      <button onClick={() => setPage("home")} className='font-bold cursor-pointer'>Home</button>
+
+      <button onClick={() => setPage("games")} className='font-bold cursor-pointer'>Games</button>
+
+      <button onClick={() => setPage("wishlist")} className='font-bold cursor-pointer ml-auto bg-amber-100 text-black p-2 rounded-xl'>Wishlist</button>
+    </nav>
+    <main className='m-3'>
+      {page === "home" && <MainPage />}
+      {page === "wishlist" && <WishlistPage />}
+      {page === "games" && <p className='font-bold'>Games coming soon!</p>}
+    </main>
   </>)
 }
---
+++ src/components/WishlistPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function WishlistPage() {
+  return (<>
+    <p className='font-bold'>Game coming soon!</p>
+  </>)
+}
