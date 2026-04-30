129c4d24a18af26a17826e4f7d69dbed4bc059b7
Nameless <nameless@thenamelessdev.com>
2026-04-30 22:22:28 +0200

    feat: make gradient to secret page

--- src/components/secretPage.tsx
+++ src/components/secretPage.tsx
@@ -1,5 +1,5 @@
 export default function SecretPage() {
   return (<>
-    <p className="font-bold text-xl">Meow</p>
+    <p className="font-bold text-xl w-fit p-3 rounded-xl" style={{ background: "linear-gradient(90deg, red, orange, yellow, green, blue, violet)" }}>Meow</p>
   </>)
 }
