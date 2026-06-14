e880b4eabc65cd32f5b2d5c0aa189260dc9f4ccc
Nameless <nameless@thenamelessdev.com>
2026-06-14 19:43:07 +0200

    feat: add some top margin in games

--- src/components/GamesPage.tsx
+++ src/components/GamesPage.tsx
@@ -9,7 +9,7 @@ export default function GamesPage() {
         <p className="text-gray-400">A 2D survival game where you play as an unemployed young adult. You start with nothing, but you work your way up through technology without ever getting a job.</p>
     </div>
 
-    <div className="text-center border-2 border-gray-800 w-50 p-3 rounded-xl">
+    <div className="text-center border-2 border-gray-800 w-50 p-3 rounded-xl mt-5">
         <p className="font-bold text-xl">Something</p>
         <hr className="-mx-3 text-gray-800" />
         <p className="text-gray-400">A story based 3D survival game.</p>
